c3691fddb7f246a3a6f91aed54b53fbc4e753c79
jwcroft <john3croft@gmail.com>
2018-07-10 17:40:24 +0200

    Updated dir_tree script.

--- scripts/create_kicad_dir_tree.sh
+++ scripts/create_kicad_dir_tree.sh
@@ -2,6 +2,17 @@
 
 # Create directory tree inside kicad project folder for better organisation.
 
-mkdir 3d_models datasheets gerber images lib_sch lib_fp.pretty pdf downloaded
+mkdir 3d_models          \
+      datasheets         \
+      gerber             \
+      images             \
+      prj_lib_sch        \
+      prj_lib_fp.pretty  \
+      pdf                \
+      downloads          \
+      
+
+
+
 
 
